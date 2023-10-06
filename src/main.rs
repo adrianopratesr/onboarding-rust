862f147141637273480f627b574c89c2dc3e4f77
Adriano Prates <adrianopratesr@gmail.com>
2023-10-06 13:55:52 -0300

    feat: projeto-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        if generator_todo() {
+            break;
+        }
+    }
+}
+
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+}
+
+fn generator_todo() -> bool {
+    println!("Você deseja criar um novo TODO? (insira apenas n ou s)");
+
+    let response = input();
+
+    if response == "n" {
+        println!("Tchau brigado");
+        true
+    } else if response == "s" {
+        println!("Qual o seu todo?");
+        let todo = input();
+        println!("Seu TODO é: {}", todo);
+        false
+    } else {
+        println!("Insira apenas 's' para sim e 'n' para não");
+        false
+    }
 }
