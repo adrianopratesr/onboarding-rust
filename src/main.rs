ce95d8424345b2481595c20e5ad0d4be15cbc7e3
Adriano Prates <adrianopratesr@gmail.com>
2023-10-21 16:07:09 -0300

    feat: add project 2

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
+use std::io::{Stdin, Stdout, Write};
+
 fn main() {
+    let mut terminal = Terminal::new();
     loop {
-        if generator_todo() {
-            break;
+        terminal.ask_if_new_todo();
+        let new_todo = terminal.ask_for_new_todo();
+        terminal.show_todo(&new_todo);
+    }
+}
+
+struct Terminal {
+    stdin: Stdin,
+    stdout: Stdout,
+}
+
+impl Terminal {
+    fn new() -> Self {
+        Terminal {
+            stdin: std::io::stdin(),
+            stdout: std::io::stdout(),
         }
     }
-}
 
-fn input() -> String {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).unwrap();
-    buf.trim().to_string()
-}
-
-fn generator_todo() -> bool {
-    println!("Você deseja criar um novo TODO? (insira apenas n ou s)");
-
-    let response = input();
-
-    if response == "n" {
-        println!("Tchau brigado");
-        true
-    } else if response == "s" {
-        println!("Qual o seu todo?");
-        let todo = input();
-        println!("Seu TODO é: {}", todo);
-        false
-    } else {
-        println!("Insira apenas 's' para sim e 'n' para não");
-        false
+    fn input(&self) -> String {
+        let mut buf = String::new();
+        self.stdin.read_line(&mut buf).unwrap();
+        buf.trim().to_string()
     }
+
+    fn ask_for_new_todo(&mut self) -> Todo {
+        println!("Qual será seu novo todo? 💬");
+        let todo_name = self.input();
+        Todo::new(todo_name)
+    }
+    fn ask_if_new_todo(&self) -> bool {
+        println!("Você deseja criar um novo TODO? ⛏  (insira apenas n ou s)");
+        let response = self.input();
+
+        loop {
+    
+            if response == "s" {
+                break;
+            } else if response == "n" {
+                println!("Tchau brigado 🙃");
+                std::process::exit(0);
+            } else {
+                println!("Resposta inválida. Insira apenas 's' para sim ou 'n' para não.");
+                self.ask_if_new_todo();
+                break;
+            }
+        }
+        true
+    }
+    fn show_todo(&mut self, todo: &Todo) {
+        println!("Qual o seu todo?");
+        writeln!(self.stdout, "Seu todo é: {}", todo.message).unwrap();
+    }
+}
+
+impl Todo {
+    const fn new(message: String) -> Self {
+        Self { message }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Todo {
+    message: String,
 }
